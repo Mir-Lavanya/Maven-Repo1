b4e3f53e77a0bbfb35b8eb820b0321fa11114e5b
Lavanya <mirejepallilavanya@gmail.com>
2024-12-31 15:38:07 +0530

    all ae committed

---
+++ src/main/java/com/example/demo_warsample1/DemoWarsample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_warsample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWarsample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWarsample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_warsample1/DemoWarsample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_warsample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWarsample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
